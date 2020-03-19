60842ea9522a4ed60cfc18cd3435ea79fda23bea
Jessica Maple <maple@jessicas-mbp-4.attlocal.net>
2020-03-19 07:35:14 -0400

    added on click to script.js

---
+++ public/assets/js/script.js
@@ -0,0 +1,23 @@
+$("#addBurger").on("click",function(){
+    var newBurger = {
+        burger_name:$("#burgerName").val()
+    }
+    $.ajax({
+        url:"/api/burgers", 
+        method:"POST",
+        data:newBurger
+    }).then(function(data){
+        location.reload()
+
+    })
+})
+
+$(".devoured").on("click",function(){
+    var id = $(this).attr("data-id")
+    $.ajax({
+        url:"/api/burgers/"+id,
+        method: "PUT"
+    }).then(function(data){
+        location.reload
+    })
+})
