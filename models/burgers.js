4b3d8b22417883da233e7e8b8e08661280a6bb18
Jessica Maple <maplejessbeth@gmail.com>
2020-05-02 12:00:09 -0400

    updated CSS

--- models/burgers.js
+++ models/burgers.js
@@ -1,20 +1,25 @@
-var orm = require("../config/orm")
-var burger={
-    selectAll: function( cbController ){
-    orm.selectAll("burgers",function(data){
-        cbController(data)
-    })
-    }, 
-    insertOne:function(newBurger ,cbController){
-        orm.insertOne("burgers",["burger_name", "devoured"],[newBurger, false],function(data){
-            cbController(data)
-        })
-    },
-    updateOne:function(id, cbController){
-        orm.updateOne("burgers",["devoured", "id"],[id,true], function(data){
-            cbController(data)
-        })
-    }
-}
-// creating ORM , how sequelize came to be 
-module.exports = burger
+var orm = require("../config/orm");
+var burger = {
+  selectAll: function(cbController) {
+    orm.selectAll("burgers", function(data) {
+      cbController(data);
+    });
+  },
+  insertOne: function(newBurger, cbController) {
+    orm.insertOne(
+      "burgers",
+      ["burger_name", "devoured"],
+      [newBurger, false],
+      function(data) {
+        cbController(data);
+      }
+    );
+  },
+  updateOne: function(id, cbController) {
+    orm.updateOne("burgers", ["devoured", "id"], [id, true], function(data) {
+      cbController(data);
+    });
+  }
+};
+// creating ORM , how sequelize came to be
+module.exports = burger;
